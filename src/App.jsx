d4e47c9a6fd96f45a8c2fc3a61f7522a2a775fee
Abhay Kamath <abhaykamath2299@gmail.com>
2023-12-26 19:16:12 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div id="root-container">
+      <iframe
+        src="https://www.youtube.com/embed/IitIl2C3Iy8?si=tUcKj28ZXjzz8UC6&amp;controls=0&scalex=2&scaley=2&originx=0&originy=0"
+        title="YouTube video player"
+        frameborder="0"
+        allow=""
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
